b5e37ea4a73bab687794ca1e16fbe1daaeec6de9
arivin <btc022003@gmail.com>
2017-06-17 16:32:04 +0800

    完善点击一个故事之后弹窗的功能,弹窗加载本地样式文件存在问题

--- api/books.js
+++ api/books.js
@@ -3,10 +3,20 @@ const axios = require('axios')
 const bookServices = {}
 
 /**
- * 调用数据接口
+ * 调用数据列表
  */
 bookServices.getAllBooks = function(page=1,cb){
     axios.get('http://penkuoer.com/api/v1/book/get_books_by_page.json?page='+page)
+        .then(res=>{
+            cb(res.data)
+        })
+}
+
+/**
+ * 获取详情
+ */
+bookServices.getDetail = function(id,cb){
+    axios.get('http://penkuoer.com/api/v1/book/get_book_detail.json?id='+id)
         .then(res=>{
             cb(res.data)
         })
--- render/index.js
+++ render/index.js
@@ -1,5 +1,7 @@
-var ejs = require('ejs')//使用ejs模版
-var Api = require('../api/books')
+const ejs = require('ejs')//使用ejs模版
+const Api = require('../api/books')
+const BrowserWindow = require('electron').remote.BrowserWindow
+
 Api.getAllBooks(1,res=>{
     // console.log(res.data)
     // 注意渲染试图的路径实在当前文件夹下
@@ -7,3 +9,22 @@ Api.getAllBooks(1,res=>{
         document.getElementById('app').innerHTML = strHtml
     })
 })
+
+
+document.body.onclick = function(e){
+    let target = e.target
+    if(target.classList.contains('book-item')){
+        // alert(target.dataset.id)
+        Api.getDetail(target.dataset.id,res=>{
+            ejs.renderFile('./views/books/detail.ejs',{article:res.data},(err,strHtml)=>{
+                let win = new BrowserWindow({width:500,height:300})
+                win.setTitle(res.data.title)
+                win.loadURL('data:text/html;charset=utf-8,' + encodeURI(strHtml)) //把模版html文件加载在窗口中
+                win.on('close',()=>{
+                    win = null
+                })
+                win.show()
+            })
+        })
+    }
+}
